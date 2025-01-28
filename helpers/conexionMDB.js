fac2103b5c20dc68f2ad18328fe213b90033caaf
Damian2891 <dbarahonag3@est.ups.edu.ec>
2025-01-28 16:24:16 -0500

    Nueva ruta

--- helpers/conexionMDB.js
+++ helpers/conexionMDB.js
@@ -1,8 +1,8 @@
 import mongoose  from "mongoose";
 export const conexion=async(n)=>{
     try{
-       // await mongoose.connect("mongodb+srv://Damian:Damian1234@cluster0.ppcz5.mongodb.net/BDDeber");
-        await mongoose.connect("mongodb://localhost:27017/BDDeber")
+        await mongoose.connect("mongodb+srv://Damian:Damian1234@cluster0.ppcz5.mongodb.net/BDDeber");
+       // await mongoose.connect("mongodb://localhost:27017/BDDeber")
         console.log ("Conectado correctamente...!");
     }catch(e){
         console.log(e);
